8166ccd08b8c97ddc51069393b97625ae18b9f6a
Alexey Zapromyotov <ZAAL6006@MUHCAD.MUHCFRD.CA>
2021-04-21 16:09:30 -0400

    Changed app config and MAIN class to make it work Tomcat app

--- src/main/java/ca/rtss/ldaptest/LdaptestApplication.java
+++ src/main/java/ca/rtss/ldaptest/LdaptestApplication.java
@@ -2,10 +2,17 @@ package ca.rtss.ldaptest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LdaptestApplication {
+public class LdaptestApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LdaptestApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LdaptestApplication.class, args);
 	}
